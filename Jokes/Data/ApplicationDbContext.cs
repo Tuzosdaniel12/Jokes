aa3d6f5e0337fa52f7ab3caad04ff8fc39ae9235
daniel <danielsoledad@gmail.com>
2021-08-27 02:39:03 -0700

    Created new table Jokes under models and Created a new view Jokes

--- Jokes/Data/ApplicationDbContext.cs
+++ Jokes/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Jokes.Models;
 
 namespace Jokes.Data
 {
@@ -12,5 +13,6 @@ namespace Jokes.Data
             : base(options)
         {
         }
+        public DbSet<Jokes.Models.Joke> Joke { get; set; }
     }
 }
